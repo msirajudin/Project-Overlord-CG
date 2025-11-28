729908149e87b4bddb2c59f7a81b4d8c336eeb47
Muhammad Sirajudin Suyuti <msirajudinsuyuti@gmail.com>
2025-11-28 14:28:31 +0700

    Initial commit: Setup folder structure and assets

---
+++ index.js
@@ -0,0 +1,10 @@
+/* Group Forming - Group 3
+No	NIM	Name
+1	2702309984	Muhammad Sirajudin Suyuti
+2	2702309901	Angel Felicia
+3	2702216002	Fredrick Willson Makmun
+4	2702316176	Dimas Alifio Paramudya Purboyo */
+
+import * as THREE from "./Three JS/build/three.module.js"
+
+const scene = new THREE.Scene();
