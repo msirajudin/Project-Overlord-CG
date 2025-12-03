50c663c6950eea038b7419e6de738ed84b8fb1c6
Muhammad Sirajudin Suyuti <msirajudinsuyuti@gmail.com>
2025-12-03 11:47:07 +0700

    fix: atur posisi Hamsuke dan momonga, add texture ground

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ gltfLoader.load('./assets/models/momonga_ainz_ooal_gown/scene.gltf',
     (gltf) => {
         const model = gltf.scene;
         //requirements ada di soal hal 7
-        model.position.set(0, -0.01, 3);
+        model.position.set(0, -0.51, 3);
         model.scale.set(0.01, 0.01, 0.01);
         model.rotation.set(0, Math.PI/2, 0);
 
@@ -90,6 +90,68 @@ gltfLoader.load('./assets/models/momonga_ainz_ooal_gown/scene.gltf',
         console.error("gagal mengsummon Momonga :(", error);
     }
 );
+
+//Ground/Tanah
+function createGround() {
+    const geo = new THREE.BoxGeometry(25, 25); 
+    const txtLoader = new THREE.TextureLoader();
+    
+    const grassTex = txtLoader.load('./assets/textures/grass/rocky_terrain_02_diff_1k.jpg');
+    
+    grassTex.wrapS = THREE.RepeatWrapping;
+    grassTex.wrapT = THREE.RepeatWrapping;
+    grassTex.repeat.set(20, 20); 
+
+    const mat = new THREE.MeshStandardMaterial({ 
+        map: grassTex,
+        roughness: 0.8 
+    });
+
+    const mesh = new THREE.Mesh(geo, mat);
+    mesh.rotation.x = -Math.PI / 2; 
+    mesh.receiveShadow = true;      
+    mesh.position.set(0, -1, 0)
+
+    scene.add(mesh);
+    console.log("Ground deployed!");
+
+}
+createGround(); 
+
+//Hamsuke atau Hamster
+function createHamsuke() {
+    const hamsukeGroup = new THREE.Group();
+
+    const texLoader = new THREE.TextureLoader();
+    const texFront = texLoader.load('./assets/textures/hamsuke/front_happy.png');
+    const texSide = texLoader.load('./assets/textures/hamsuke/side.png');
+    const texTopBack = texLoader.load('./assets/textures/hamsuke/top&back.png');
+    
+    const bodyMaterials = [
+        new THREE.MeshStandardMaterial({ map: texSide }),        
+        new THREE.MeshStandardMaterial({ map: texSide }),      
+        new THREE.MeshStandardMaterial({ map: texTopBack }),     
+        new THREE.MeshStandardMaterial({ map: texTopBack }),    
+        new THREE.MeshStandardMaterial({ map: texFront }),      
+        new THREE.MeshStandardMaterial({ map: texTopBack })     
+    ];
+
+    const bodyGeo = new THREE.BoxGeometry(3, 2.5, 3); 
+    const bodyMesh = new THREE.Mesh(bodyGeo, bodyMaterials);
+    
+    bodyMesh.castShadow = true;
+    bodyMesh.receiveShadow = true;
+    
+    hamsukeGroup.add(bodyMesh);
+
+    return hamsukeGroup;
+}
+
+const hamsuke = createHamsuke();
+
+hamsuke.position.set(1.8, 0.75, -1.95); 
+hamsuke.rotation.set(0, 0.5, 0); 
+scene.add(hamsuke);
 
 window.addEventListener('resize', function(){
     renderer.setSize(window.innerWidth, window.innerHeight);
